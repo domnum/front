43a15b821d4dd8b806ba248ff0cd67c2a23420fe
KaueSMoraes <kaue040803@gmail.com>
2025-04-18 17:42:34 -0400

    ajustes header

--- src/app/layouts/ladingpage-layout/landingpage-layout.component.ts
+++ src/app/layouts/ladingpage-layout/landingpage-layout.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { BaseComponent } from '../../shared/base/base.component';
 import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { ToggleSwitch } from 'primeng/toggleswitch';
 import { LogoComponent } from "../../components/logo/logo.component";
+import { FormsModule } from '@angular/forms'; // 👈 Import necessário para ngModel
 
 @Component({
   selector: 'app-landingpage-layout',
+  standalone: true,
   imports: [
     RouterModule,
     MatIcon,
-    LogoComponent
-],
+    ToggleSwitch,
+    LogoComponent,
+    FormsModule 
+  ],
   templateUrl: './landingpage-layout.component.html',
   styleUrl: './landingpage-layout.component.css'
 })
-export class LandingpageLayoutComponent extends BaseComponent{
-
-}
+export class LandingpageLayoutComponent extends BaseComponent {}
